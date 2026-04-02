e20507b356d6ec1bb37b8dd36ec3cafe174b9053
Nick <hoernernick@web.de>
2026-04-02 10:41:47 +0200

    refactored thumbnail upload so it gets stored to disk and fetched instead of in memory

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -2,39 +2,10 @@ import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo, type Video } from "../db/videos";
 import type { ApiConfig } from "../config";
-import type { BunRequest } from "bun";
+import { fetch, type BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import * as path from "node:path";
+import { getAssetDiskPath, getAssetURL, mediaTypeExt } from "./assets";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -76,11 +47,22 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new Error("Error reading file data");
   }
   const mediaType: string = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
 
-  videoThumbnails.set(videoId, { data: fileData, mediaType: mediaType });
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Invalid file type. Only JPEG or PNG allowed.");
+  }
 
-  const thumbnailUrl = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
-  video.thumbnailURL = thumbnailUrl;
+  const ext = mediaTypeExt(mediaType);
+  const fileName = `${videoId}${ext}`;
+
+  const assetDiskPath = getAssetDiskPath(cfg, fileName);
+  await Bun.write(assetDiskPath, fileData);
+
+  const urlPath = getAssetURL(cfg, assetDiskPath);
+  video.thumbnailURL = urlPath;
 
   updateVideo(cfg.db, video);
 
